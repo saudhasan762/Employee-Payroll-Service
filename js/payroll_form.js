cd7bad9b58720478d87bb271f66e52edc99de3ae
saudhasan762 <azanali152@gmail.com>
2021-04-03 15:50:47 +0530

    Added

--- js/payroll_form.js
+++ js/payroll_form.js
@@ -4,3 +4,22 @@ output.textContent = salary.value;
 salary.addEventListener('input', function() {
     output.textContent = salary.value;
 });
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    const name = document.querySelector('#name');
+    const textError = document.querySelector('.text-error')
+    name.addEventListener('input', function() {
+        if(name.value.length == 0) {
+            textError.textContent = "";
+            return;
+        }
+        try {
+            (new EmployeePayrollData()).name = name.value;
+            textError.textContent = "";
+        } catch (e) {
+            textError.textContent = e;
+        }
+    });
+});
+
+const save = 
